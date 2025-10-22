4f5229225b49381bc1383d767ff4ba4f4e3f5a46
shef735 <shef735@gmail.com>
2025-10-22 17:07:32 +0800

    oct 22

--- old_system/admin/source/includes/footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="codex-footer">
-        <p>M-One Solutions Copyright 2024-25 ©  All rights reserved.</p>
-      </footer>
-     
--- old_system/report-all/index_main.php
+++
@@ -1,5 +0,0 @@
-<?php
-	// include ("../../update_transaction_full.php"); <?php 
-	//include ("../../update_masterfile_stocks.php");
-	 include ("index.php"); 
-?>
